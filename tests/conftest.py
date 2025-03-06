a6f8e6b5c7e413a4ee7f280636feb3f378b37b1e
Dark-eXe <andrewtur510@gmail.com>
2025-03-06 04:07:57 -0800

    Update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 from src import AcroDB
 
+table_name = "MAG_Code-of-Points"
+bucket_name = "dsci551-acrobucket"
+
 @pytest.fixture
 def test_object():
-    return AcroDB(table_name="MAG_Code-of-Points", bucket_name="dsci551-acrobucket")
+    return AcroDB(table_name=table_name, bucket_name=bucket_name)
+    
+@pytest.fixture
+def test_mvtId():
+    return "1"
