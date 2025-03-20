7b744620b6ba1932c9f73dcb7d71740415483d7d
Dark-eXe <andrewtur510@gmail.com>
2025-03-20 15:28:01 -0700

    Add script demo for chat_query

---
+++ demo/chat_query.py
@@ -0,0 +1,33 @@
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+try:
+    from src.AcroDB import AcroDB
+    from src.ChatDB import ChatDB
+except ModuleNotFoundError:
+    module_path = os.path.abspath(os.path.join('.'))
+    if module_path not in sys.path:
+        sys.path.append(module_path)
+    from src.AcroDB import AcroDB
+    from src.ChatDB import ChatDB
+
+def main():
+    # AcroDB
+    table_name, bucket_name = "MAG_Code-of-Points", "dsci551-acrobucket"
+    my_acro1 = AcroDB(table_name=table_name, bucket_name=bucket_name)
+
+    table_name, bucket_name = "Parkourpedia", "dsci551-acrobucket"
+    my_acro2 = AcroDB(table_name=table_name, bucket_name=bucket_name)
+
+    # ChatDB
+    my_chat = ChatDB(acrodb_list=[my_acro1, my_acro2])
+    if my_chat.set_api_key(API_KEY=open("secrets/API_KEY" if os.path.basename(os.getcwd()) == "AcroDB" else "../secrets/API_KEY").read()):
+        print("OpenAI client setup successful!")
+
+    my_chat.loop()
+
+if __name__ == "__main__":
+    main()
